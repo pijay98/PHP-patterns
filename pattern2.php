ef24735a65773e89311a69d89dfa487695e500bd
pijay98 <prithvijit1998@gmail.com>
2023-06-09 19:14:56 +0400

    Added

---
+++ pattern2.php
@@ -0,0 +1,30 @@
+<?php  
+for($i=5;$i>=1;$i--){
+	for($j=1;$j<=$i;$j++){
+	echo " * ";
+	}
+	echo '</br>';
+}
+
+?>
+
+
+<?php
+for($i=5;$i>=1;$i--){
+	for($j=1;$j<=$i;$j++){
+	echo " $i ";
+	}
+	echo '</br>';
+}
+
+?>
+
+<?php
+for($i=5;$i>=1;$i--){
+	for($j=1;$j<=$i;$j++){
+	echo " $j ";
+	}
+	echo '</br>';
+}
+
+?>
